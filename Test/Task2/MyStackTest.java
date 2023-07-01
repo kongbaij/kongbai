cb2e2e3c85447667182cd59526e226a1f98b0f46
kongbaij <3437255635@qq.com>
2023-07-01 11:24:16 +0800

    第二阶段任务测试源文件

--- Test/Task2/MyStackTest.java
+++ Test/Task2/MyStackTest.java
@@ -1,4 +1,43 @@
+package Task2;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MyStackTest {
-  
+
+    @Test
+    public void test_String() {
+        MyStack<String> myStack=new MyStack<>();
+        myStack.push("dhd");
+        myStack.push("sdg");
+        myStack.push("55");
+        assertEquals("55",myStack.pop());
+        assertEquals("sdg",myStack.top());
+        assertEquals("sdg",myStack.pop());
+        assertEquals("dhd",myStack.pop());
+        assertTrue(myStack.deque.isEmpty());
+    }
+
+    @Test
+    void test_Integer() {
+        MyStack<Integer> myStack=new MyStack<>();
+        myStack.push(55);
+        myStack.push(16);
+        assertEquals(16,myStack.pop());
+        assertEquals(55,myStack.top());
+        assertEquals(55,myStack.pop());
+        assertTrue(myStack.deque.isEmpty());
+    }
+
+    @Test
+    void test_Double() {
+        MyStack<Double> myStack=new MyStack<>();
+        myStack.push(11.6);
+        myStack.push(55.32);
+        assertEquals(55.32,myStack.pop());
+        assertEquals(11.6,myStack.top());
+        assertEquals(11.6,myStack.pop());
+        assertTrue(myStack.deque.isEmpty());
+    }
 }
